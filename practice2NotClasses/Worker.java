4e4c25ac1e83a36cf01218d140bde723ffa431df
dmitriKON <100063965+dmitriKON@users.noreply.github.com>
2022-07-14 15:23:28 +0300

    Add files via upload
    This time not classes

---
+++ practice2NotClasses/Worker.java
@@ -0,0 +1,37 @@
+package practice2NotClasses;
+
+public class Worker extends Thread{
+
+    private int id;
+    private Data data;
+
+    public Worker(int id, Data d) {
+        this.id = id;
+        data = d;
+        this.start();
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        for (int i = 0; i < 5; i++) {
+            synchronized (data) {
+                try {
+                    while (id != data.getState()) {
+                        data.wait();
+                    }
+                    if (id == 1) {
+                        data.Tic();
+                    } else if (id == 2) {
+                        data.Tak();
+                    } else {
+                        data.Toy();
+                    }
+                    data.notifyAll();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
